b0fafc14f603403503173d458a32a890b9fb9f4a
Nikhil Kumar <nikhilkumar9.kumar@gmail.com>
2022-03-15 23:47:49 +0530

    Bioler Plate Code

---
+++ api_v1.py
@@ -0,0 +1,13 @@
+from sanic import Blueprint
+
+from constant import *
+
+API_VERSION = 'v1'
+
+
+def setup_api(app):
+    api_prefix = f'/{SERVICENAME.lower()}/{API_VERSION}'
+    api_v1 = Blueprint(API_VERSION, url_prefix=api_prefix)
+
+    # api_v1.add_route(RegisterView.as_view(), '/register', strict_slashes=False)
+    app.blueprint(api_v1)
---
+++ constant.py
@@ -0,0 +1,7 @@
+SERVICENAME = 'booking'
+DB_CONN_URI = "mongodb://localhost:27017/imock?retryWrites=true&w=majority"
+DB_BASIC_CONN_URI = "mongodb://localhost:27017/"
+DB_NAME = "imock"
+USER_COLLECTION = ""
+USER_ID_TAG = "id"
+OBJECT_ID_TAG = "_id"

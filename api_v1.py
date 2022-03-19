a7bd68f3fad86796d5d236fa8fd871b3bf169004
Nikhil Kumar <nikhilkumar9.kumar@gmail.com>
2022-03-19 23:10:10 +0530

    Booking View Addition

--- api_v1.py
+++ api_v1.py
@@ -1,6 +1,7 @@
 from sanic import Blueprint
 
 from constant import *
+from views.booking_view import BookingView
 
 API_VERSION = 'v1'
 
@@ -9,5 +10,5 @@ def setup_api(app):
     api_prefix = f'/{SERVICENAME.lower()}/{API_VERSION}'
     api_v1 = Blueprint(API_VERSION, url_prefix=api_prefix)
 
-    # api_v1.add_route(RegisterView.as_view(), '/register', strict_slashes=False)
+    api_v1.add_route(BookingView.as_view(), '/register', strict_slashes=False)
     app.blueprint(api_v1)
